5ed2c43e4c97cc0af19fde1bb3e527f9b9a38153
hamza-sadeq <hamzasadeq41@gmail.com>
2022-11-04 20:57:15 +0200

    task.js

---
+++ index.js
@@ -0,0 +1,109 @@
+function openNav(){
+    document.getElementById("sidebar").style.width = "250px"
+}
+
+function closeNav(){
+    document.getElementById("sidebar").style.width = "0"
+}
+
+function lightTheme(){
+    var BODY = document.body;
+    BODY.classList.toggle("light-mode");
+    BODY.style.color = "black";
+    var Nav = document.getElementsByTagName("span")[0];
+    Nav.style.background = "white";
+    Nav.style.color = "black";
+    var Nav = document.getElementsByTagName("span")[1];
+    Nav.style.background = "white";
+    Nav.style.color = "black";
+    var Nav = document.getElementsByTagName("span")[2];
+    Nav.style.background = "white";
+    Nav.style.color = "black";
+    var Nav = document.getElementsByTagName("span")[3];
+    Nav.style.background = "white";
+    Nav.style.color = "black";
+    var Nav = document.getElementsByTagName("span")[4];
+    Nav.style.background = "white";
+    Nav.style.color = "black";
+    document.getElementById("icon").style.backgroundColor = "black";
+    document.getElementById("sidebar").style.backgroundColor = "black";
+    var Aside = document.getElementsByTagName("a")[0];
+    Aside.style.color = "white";
+    document.getElementById("link").style.color = "white";
+    document.getElementById("link1").style.color = "white";
+    document.getElementById("link2").style.color = "white";
+    document.getElementById("link3").style.color = "white";
+    document.getElementById("link4").style.color = "white";
+    document.getElementById("apple").style.color = "black";
+    
+    document.getElementById("iphone").style.color = "black";
+    document.getElementById("lorem").style.color = "black";
+}
+
+function white(){
+    var BODY = document.body;
+    BODY.classList.toggle("light-mode");
+    BODY.style.color = "black";
+    var Nav = document.getElementsByTagName("span")[0];
+    Nav.style.background = "white";
+    Nav.style.color = "black";
+    var Nav = document.getElementsByTagName("span")[1];
+    Nav.style.background = "white";
+    Nav.style.color = "black";
+    var Nav = document.getElementsByTagName("span")[2];
+    Nav.style.background = "white";
+    Nav.style.color = "black";
+    var Nav = document.getElementsByTagName("span")[3];
+    Nav.style.background = "white";
+    Nav.style.color = "black";
+    var Nav = document.getElementsByTagName("span")[4];
+    Nav.style.background = "white";
+    Nav.style.color = "black";
+    document.getElementById("icon").style.backgroundColor = "black";
+    document.getElementById("sidebar").style.backgroundColor = "black";
+    var Aside = document.getElementsByTagName("a")[0];
+    Aside.style.color = "white";
+    document.getElementById("link").style.color = "white";
+    document.getElementById("link1").style.color = "white";
+    document.getElementById("link2").style.color = "white";
+    document.getElementById("link3").style.color = "white";
+    document.getElementById("link4").style.color = "white";
+    document.getElementById("apple").style.color = "black";
+    
+    document.getElementById("iphone").style.color = "black";
+    document.getElementById("lorem").style.color = "black";
+}
+
+function pink(){
+    var BODY = document.body;
+    BODY.classList.toggle("light-pink");
+    BODY.style.color = "pink";
+    var Nav = document.getElementsByTagName("span")[0];
+    Nav.style.background = "pink";
+    Nav.style.color = "black";
+    var Nav = document.getElementsByTagName("span")[1];
+    Nav.style.background = "pink";
+    Nav.style.color = "black";
+    var Nav = document.getElementsByTagName("span")[2];
+    Nav.style.background = "pink";
+    Nav.style.color = "black";
+    var Nav = document.getElementsByTagName("span")[3];
+    Nav.style.background = "pink";
+    Nav.style.color = "black";
+    var Nav = document.getElementsByTagName("span")[4];
+    Nav.style.background = "pink";
+    Nav.style.color = "black";
+    document.getElementById("icon").style.backgroundColor = "black";
+    document.getElementById("sidebar").style.backgroundColor = "black";
+    var Aside = document.getElementsByTagName("a")[0];
+    Aside.style.color = "white";
+    document.getElementById("link").style.color = "white";
+    document.getElementById("link1").style.color = "white";
+    document.getElementById("link2").style.color = "white";
+    document.getElementById("link3").style.color = "white";
+    document.getElementById("link4").style.color = "white";
+    document.getElementById("apple").style.color = "black";
+    
+    document.getElementById("iphone").style.color = "black";
+    document.getElementById("lorem").style.color = "black";
+}
